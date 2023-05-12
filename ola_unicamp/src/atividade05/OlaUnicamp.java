f4a71ff31fbae845f9519447912a3b867abfe542
Thiago Lima <39269384+ThiagoLi@users.noreply.github.com>
2023-05-12 20:22:08 -0300

    Add files via upload

---
+++ ola_unicamp/src/atividade05/OlaUnicamp.java
@@ -0,0 +1,10 @@
+package atividade05;
+
+public class OlaUnicamp {
+
+	public static void main(String[] args) {
+		System.out.print("Ola Unicamp!");
+
+	}
+
+}
---
+++ ola_unicamp/src/module-info.java
@@ -0,0 +1,2 @@
+module ola_unicamp {
+}
